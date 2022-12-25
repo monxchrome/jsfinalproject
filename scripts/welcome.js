e57b0715e40e8cc1a35a8eb59f8182a8082225dd
monxchrome <inacheat@gmail.com>
2022-12-25 13:24:40 +0100

    added comments

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,5 +1,6 @@
-let btn = document.getElementById('btn');
+let btn = document.getElementById('btn-div');
 let wel = document.getElementById('wel');
+
 btn.onclick = function () {
     btn.classList.add('lineDown');
     wel.classList.add('lineDown');
